7d649c224c49a22810b030f9587857a2b16dc4b7
Keith Mngadi <38478562+KeithMatic@users.noreply.github.com>
2024-11-11 21:02:30 +0200

    Completed exercise

--- CH02/EX2.4.py
+++ CH02/EX2.4.py
@@ -1,17 +1,22 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "icecream",
+#     "ruff",
+# ]
+# ///
+
 # (Convert pounds into kilograms) Write a program that converts pounds into
 # kilograms. The program prompts the user to enter a value in pounds, converts it to
 # kilograms, and displays the result. One pound is 0.454 kilograms. Here is a
 # sample run:
 
+from icecream import ic
 
-# Prompt the user to enter a value in pounds
+KILOGRAM = 0.454
+
 pounds = float(input("Enter a value in pounds: "))
 
-# One pound is 0.454 kilograms
-kilograms = 0.454
+kilograms = pounds * KILOGRAM
 
-# Convert the pounds to kilograms
-kilograms *= pounds
-
-# Display results
-print(f"{pounds} is {kilograms} kilograms")
+ic(f"{pounds} pounds is {kilograms} kilograms")
