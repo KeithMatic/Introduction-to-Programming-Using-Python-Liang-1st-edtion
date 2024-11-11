a70c3673a57910591399ee78c0b203a5293a7028
Keith Mngadi <38478562+KeithMatic@users.noreply.github.com>
2024-11-12 01:25:28 +0200

    :art: Completed exercise

--- CH02/EX2.7.py
+++ CH02/EX2.7.py
@@ -1,9 +1,24 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "icecream",
+# ]
+# ///
+
 # (Find the number of years and days) Write a program that prompts the user to
 # enter the minutes (e.g., 1 billion), and displays the number of years and days for
 # the minutes.
 
-min = int(input("Enter the number of minutes: "))
-years = int(min / (365 * 24 * 60))
-remainingMin = min % (365 * 24 * 60)
-days = int(remainingMin / (24 * 60))
-print(min, "minutes is approximately", years, "years and", days, "days")
+from icecream import ic  # type: ignore
+
+minutes = int(input("Enter the number of minutes: "))
+
+hours = minutes // 60
+
+total_days = hours // 24
+
+years = total_days // 365
+
+days = total_days % 365
+
+ic(f"{minutes} minutes is approximately {years} years and {days} days")
