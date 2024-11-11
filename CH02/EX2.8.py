f1fa6fef732fa1ff2b770db1042e5e26dfe08219
Keith Mngadi <38478562+KeithMatic@users.noreply.github.com>
2024-11-12 01:49:42 +0200

    :art: Completed exercise

--- CH02/EX2.8.py
+++ CH02/EX2.8.py
@@ -1,3 +1,11 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "icecream",
+# ]
+# ///
+
+
 # (Science: calculate energy) Write a program that calculates the energy needed to
 # heat water from an initial temperature to a final temperature. Your program should
 # prompt the user to enter the amount of water in kilograms and the initial and final
@@ -6,8 +14,12 @@
 # where M is the weight of water in kilograms, temperatures are in degrees Celsius,
 # and energy Q is measured in joules.
 
-water = eval(input("Enter the amount of water in kilograms:"))
-initTemp = eval(input("Enter the initial temperature:"))
-finTemp = eval(input("Enter the final temperature:"))
-q = water * (finTemp - initTemp) * 4184
-print("The energy needed is",q)
+from icecream import ic  # type: ignore
+
+water_in_kilograms = float(input("Enter the amount of water in kilograms: "))
+initial_temperature = float(input("Enter initial temperatures: "))
+final_temperature = float(input("Enter final temperatures: "))
+
+energy = water_in_kilograms * (final_temperature - initial_temperature) * 4184
+
+ic(f"The energy needed is {energy}")
