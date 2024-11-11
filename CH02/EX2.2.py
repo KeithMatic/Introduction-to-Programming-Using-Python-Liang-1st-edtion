34f136c153ecdc6c304abe27a9b34010ed120e71
Keith Mngadi <38478562+KeithMatic@users.noreply.github.com>
2024-11-11 20:26:01 +0200

    Completed exercise

--- CH02/EX2.2.py
+++ CH02/EX2.2.py
@@ -1,19 +1,22 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "icecream",
+# ]
+# ///
 # (Compute the volume of a cylinder) Write a program that reads in the radius and
 # length of a cylinder and computes the area and volume using the following formulas:
 # area = radius * radius * π
 # volume = area * length
 
+import math
 
-PI = 3.14159
+from icecream import ic  # type: ignore
 
 radius, length = eval(input("Enter the radius and length of a cylinder: "))
 
-# area = radius * radius * pi
-area = float(radius * radius) * PI
-
-# volume = area * length
+area = radius * radius * math.pi
 volume = area * length
 
-# print area and length
-print(f"The area is {round(area)}")
-print(f"The volume  is {round(volume)}")
+ic(f"The area is {area}")
+ic(f"The volume is {volume}")
