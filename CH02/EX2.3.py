a02c64d4653b286aed88cc8b5a1e1c929057f979
Keith Mngadi <38478562+KeithMatic@users.noreply.github.com>
2024-11-11 20:43:52 +0200

    :art: Completed exercise

--- CH02/EX2.3.py
+++ CH02/EX2.3.py
@@ -1,17 +1,20 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "icecream",
+# ]
+# ///
+
+
 # (Convert feet into meters) Write a program that reads a number in feet, converts it
 # to meters, and displays the result. One foot is 0.305 meters.
 
-# Prompt the user to enter value for feet
+from icecream import ic  # type: ignore
+
+FOOT = 0.305
+
 feet = float(input("Enter a value for feet: "))
 
-# One foot is 0.305 meters
-meters = 0.305
+meters = feet * FOOT
 
-# Calculate meters
-meters *= feet
-
-# Display the results
-print(f"{feet} feet is {meters} meters")
-feet = eval(input("Enter a value for feet:"))
-metre = feet * 0.305
-print(feet, "feet is", metre, "metres")
+ic(f"{feet} feet is {meters} meters")
